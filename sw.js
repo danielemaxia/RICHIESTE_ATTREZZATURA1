904e01fab62f5be2c5a5841d21af99a528de5b20
danielemaxia <danielemaxia@hotmail.it>
2026-04-27 10:44:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'richiesta-attrezzature-fix-articoli-20260427-0900';
+const FILES_TO_CACHE = ['./','./index.html','./manifest.webmanifest','./icone/icon-192.png','./icone/icon-512.png','./apple-touch-icon.png','./favicon-32.png'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE).catch(() => null)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => null);
+    return response;
+  }).catch(() => caches.match(event.request).then(response => response || caches.match('./index.html'))));
+});
